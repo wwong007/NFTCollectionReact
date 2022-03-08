87d2e26eca1030afb86a32455a0440f554453364
Will Wong <williamwj.wong@gmail.com>
2022-03-07 18:47:06 -0800

    Refactor styling in AddNFT modal related components

--- src/components/AddNFT/Form/AddNFTForm.tsx
+++ src/components/AddNFT/Form/AddNFTForm.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
+import { modalFormStyle } from '../../../core/Form/Form.styles';
 import FormPropsTextFields from '../../../core/Form/FormPropsTextFields';
 import { addNFTFormTextFields } from '../../../models/AddNFT/AddNFTForm.model';
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 540,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  display: 'grid',
-  gridTemplateColumns: 'repeat(2, auto)',
-  '& .MuiTextField-root': { m: 1, width: '25ch' },
-  '& button': { m: 1, width: '32.5ch' },
-};
 
 
 export default function AddNFTForm () {
   return (
     <FormPropsTextFields
       fieldProps={addNFTFormTextFields}
-      style={style}
+      style={modalFormStyle}
     />
   )
 }
--- src/components/AddNFT/Modal/AddNFTModal.tsx
+++ src/components/AddNFT/Modal/AddNFTModal.tsx
@@ -3,10 +3,6 @@ import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
 import AddNFTForm from '../Form/AddNFTForm';
 import BasicModal from '../../../core/Modal/BasicModal';
 
-const modalPosition: { [key: string]: string } = { 
-  gridColumn: 'col1-end/col2-end', 
-  gridRow: 'row1-start/row1-end' 
-};
 
 export default function AddNFTModal() {
 
@@ -14,7 +10,6 @@ export default function AddNFTModal() {
     <BasicModal
       buttonIcon={<AddBoxOutlinedIcon fontSize='large'/>}
       content={<AddNFTForm />}
-      style={modalPosition}
       modalTitle='Modal for Adding NFT'
       modalDescription='A modal containing a form for adding nft to the collection'
     />

a29b11513e954b83a6e3c8bc2a2b0b1ff0bcf639
Will Wong <williamwj.wong@gmail.com>
2022-03-08 14:56:57 -0800

    Refactor form comonent to take in handle change function, refactor prop type

--- src/core/Form/FormPropsTextFields.tsx
+++ src/core/Form/FormPropsTextFields.tsx
@@ -4,6 +4,7 @@ import { Box, Button, TextField } from '@mui/material';
 export interface TextFieldI {
   id: string;
   label: string;
+  name: string;
   type?: string;
   inputLabelProps?: { [key: string]: string | number | boolean };
 }
@@ -11,11 +12,12 @@ export interface TextFieldI {
 interface PropTypes {
   fieldProps: TextFieldI[];
   style: { [key: string]: string | number | boolean | { [key: string]: string | number }};
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 };
 
 
 export default function FormPropsTextFields(props: PropTypes) {
-  const { fieldProps, style } = props;
+  const { fieldProps, style, handleInputChange } = props;
   return (
     <Box
       component='form'
@@ -24,14 +26,16 @@ export default function FormPropsTextFields(props: PropTypes) {
       autoComplete='off'
     >
       {fieldProps.map((fieldProp) => {
-        const { id, label, type, inputLabelProps } = fieldProp;
+        const { id, label, type, inputLabelProps, name } = fieldProp;
         return (
           <TextField
             id={id}
+            name={name}
             key={label}
             label={label}
             type={type}
             InputLabelProps={inputLabelProps}
+            onChange={handleInputChange}
           />
         )
       })}

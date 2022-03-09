b5580bbb6c7bc7ea2d6e8f926caa9edf61b489c8
Will Wong <williamwj.wong@gmail.com>
2022-03-08 17:58:36 -0800

    Implement add NFT request factory

---
+++ src/utils/addNFTRequest.factory.ts
@@ -0,0 +1,31 @@
+import { AddNFTInputsI, AddNFTRequestI } from "../models/AddNFT/AddNFTRequest.model";
+
+function getNameInitials(name: string): string {
+  const wordArr: string[] = name.split(' ');
+  let initials: string = '';
+
+  for (let word of wordArr) {
+    initials += word[0];
+  }
+
+  return initials
+}
+
+export function addNFTRequestFactoryFromInput(inputs: AddNFTInputsI): AddNFTRequestI {
+  const { name, tokenId, priceBought, feeBought, methodBought, url } = inputs;
+  const dateBought: number = Date.parse(inputs.dateBought);
+  const initials: string = getNameInitials(name);
+  const id: string = `${initials}-${tokenId}-${dateBought}`;
+  const request: AddNFTRequestI = {
+    id,
+    name,
+    tokenId,
+    priceBought,
+    dateBought,
+    feeBought,
+    methodBought,
+    url
+  };
+
+  return request;
+}

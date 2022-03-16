a8b60a295baf7f7af51b2ffd788dff6c582f2525
Will Wong <williamwj.wong@gmail.com>
2022-03-16 15:45:28 -0700

    Add div to the components

--- src/components/Page/CollectionPage/CollectionPage.tsx
+++ src/components/Page/CollectionPage/CollectionPage.tsx
@@ -1,6 +1,11 @@
+// REACT
 import React from 'react';
+
+// MODEL
 import { NFTCollectionI } from '../../../models/NFTCollection/NFTCollection.model';
 import { DataPropI } from '../../../models/Props/props.models';
+
+// COMPONENTS
 import CollectionTable from '../../Collection/CollectionTable';
 import UtilBar from '../../UtilBar/UtilBar';
 import PageBody from '../PageBody';
@@ -9,10 +14,10 @@ import PageBody from '../PageBody';
 
 export default function CollectionPage(props: DataPropI<NFTCollectionI>) {
   return (
-    <>
+    <div className='Main-Body'>
       <PageBody
         components={[<UtilBar />, <CollectionTable data={props.data}/>]}
       />
-    </>
+    </div>
   )
 }
--- src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
+++ src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
@@ -1,19 +1,26 @@
+// REACT
 import React from 'react';
+
+// MODEL
 import { NFTCollectionI } from '../../../models/NFTCollection/NFTCollection.model';
 import { DataPropI } from '../../../models/Props/props.models';
-import { calculateInfoBarData } from '../../../utils/calculateInfoBarData';
+
+// COMPONENTS
 import SalesHistoryInfoBar, { InfoBarPropI } from '../../Bar/InfoBar/InfoBar';
 import SalesHistoryTable from '../../SalesHistory/SalesHistoryTable';
 import PageBody from '../PageBody';
+
+// UTIL
+import { calculateInfoBarData } from '../../../utils/calculateInfoBarData';
 
 export default function SalesHistoryPage(props: DataPropI<NFTCollectionI>) {
   const { data } = props;
   const infoBarData: InfoBarPropI = calculateInfoBarData(data);
   return (
-    <>
+    <div className='Main-Body'>
       <PageBody
         components={[<SalesHistoryInfoBar data={infoBarData} />, <SalesHistoryTable data={data}/>]}
       />
-    </>
+    </div>
   )
 }

a4ed5e8b2e8292a1595fa185b69675d565a4be15
Will Wong <williamwj.wong@gmail.com>
2022-03-15 17:16:03 -0700

    Implement SalesHistoryPage component

--- src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
+++ src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { NFTCollectionI } from '../../../models/NFTCollection/NFTCollection.model';
+import { DataPropI } from '../../../models/Props/props.models';
+import { calculateInfoBarData } from '../../../utils/calculateInfoBarData';
+import SalesHistoryInfoBar, { InfoBarPropI } from '../../Bar/InfoBar/InfoBar';
+import SalesHistoryTable from '../../SalesHistory/SalesHistoryTable';
+import PageBody from '../PageBody';
 
-export default function SalesHistoryPage() {
+export default function SalesHistoryPage(props: DataPropI<NFTCollectionI>) {
+  const { data } = props;
+  const infoBarData: InfoBarPropI = calculateInfoBarData(data);
   return (
     <>
+      <PageBody
+        components={[<SalesHistoryInfoBar data={infoBarData} />, <SalesHistoryTable data={data}/>]}
+      />
     </>
   )
 }

99acff251b99e730eb75c40a5a35e39d3ab6e0e0
Will Wong <williamwj.wong@gmail.com>
2022-03-22 16:21:16 -0700

    Implement search function to sale history componene

--- src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
+++ src/components/Page/SalesHistoryPage/SalesHistoryPage.tsx
@@ -12,14 +12,24 @@ import PageBody from '../PageBody';
 
 // UTIL
 import { calculateInfoBarData } from '../../../utils/calculateInfoBarData';
+import { handleSearchData } from '../../../handlers/handleSearchData';
 
 export default function SalesHistoryPage(props: DataPropI<NFTCollectionI>) {
   const { data } = props;
   const infoBarData: InfoBarPropI = calculateInfoBarData(data);
+  const [filteredData, setFilteredData] = React.useState(data);
+  const [isSearch, setIsSearch] = React.useState(false)
+
+  const handleSearch = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    handleSearchData<NFTCollectionI>(data, event, setFilteredData);
+    setIsSearch(true)
+  }
   return (
     <div className='Main-Body'>
       <PageBody
-        components={[<SalesHistoryInfoBar data={infoBarData} />, <SalesHistoryTable data={data}/>]}
+        components={[
+        <SalesHistoryInfoBar data={infoBarData} handleSearch={handleSearch}/>, 
+        <SalesHistoryTable data={isSearch ? filteredData : data}/>]}
       />
     </div>
   )
